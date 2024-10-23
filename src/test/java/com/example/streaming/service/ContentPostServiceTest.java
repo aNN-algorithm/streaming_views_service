2b4c5a11dd895085b1de026f6bc8564e26c6a447
aNN-algorithm <kyukyu0314@naver.com>
2024-10-23 19:47:02 +0900

    ✅test: #1 테스트 코드 작성

---
+++ src/test/java/com/example/streaming/service/ContentPostServiceTest.java
@@ -0,0 +1,50 @@
+package com.example.streaming.service;
+
+import com.example.mock.FakeContentPostRepository;
+import com.example.streaming.domain.ContentPost;
+import com.example.streaming.controller.request.ContentPostCreate;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class ContentPostServiceTest {
+
+    private ContentPostServiceImpl contentPostService;
+
+    @BeforeEach
+    void init() {
+
+        FakeContentPostRepository fakeContentPostRepository = new FakeContentPostRepository();
+
+        this.contentPostService = ContentPostServiceImpl.builder()
+                .contentPostRepository(fakeContentPostRepository)
+                .build();
+    }
+
+    @Test
+    public void ContentPost를_Create() {
+        // given
+        ContentPostCreate contentPostCreate = ContentPostCreate.builder()
+                .name("데이먼스 이어의 플레이리스트")
+                .adId("1,2,3")
+                .isPublic(true)
+                .build();
+
+        // when
+        ContentPost contentPost = contentPostService.create(contentPostCreate);
+
+        // then
+        assertThat(contentPost.getId()).isNotNull();
+        assertThat(contentPost.getCreatorId()).isEqualTo(1L);
+        assertThat(contentPost.getAdId()).isEqualTo("1,2,3");
+        assertThat(contentPost.getName()).isEqualTo("데이먼스 이어의 플레이리스트");
+        assertThat(contentPost.getTotalViews()).isEqualTo(0L);
+        assertThat(contentPost.getTotalAdViews()).isEqualTo(0L);
+        assertThat(contentPost.getTotalPlayTime()).isEqualTo(0L);
+        //assertThat(contentPost.getCreatedAt()).isEqualTo();
+        assertThat(contentPost.isPublic()).isEqualTo(true);
+        assertThat(contentPost.isDeleted()).isEqualTo(false);
+        assertThat(contentPost.getDeletedAt()).isNull();
+    }
+}
